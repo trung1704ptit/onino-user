0f6033568fd83d0e9576acf2a68be6d37668efcf
Trung <trung@onino.vn>
2019-10-28 11:01:46 +0700

    format notify

--- src/components/Notification/index.vue
+++ src/components/Notification/index.vue
@@ -1,22 +1,22 @@
 <template>
-  <el-dropdown trigger="click" @command="handleRemoveNotification">
-    <div>
-      <i class="el-icon-bell" />
-    </div>
-    <el-dropdown-menu slot="dropdown" class="dropdown-header-menu">
-      <el-dropdown-item v-for="item of notifications" :key="item.value" :disabled="size===item.value" :command="item.value">
-        <div class="p-15 notify-item">
-            <div class="flex">
-                <img :src="item.icon" class="notify-icon"/>
-                <div>
-                    <p class="m-0">{{ item.description }}</p>
-                    <i class="timestamp">{{ moment(item.timestamp).format('HH:MM DD/MM/YYYY') }}</i>
+    <el-dropdown trigger="click" @command="handleRemoveNotification">
+        <div>
+        <i class="el-icon-bell" />
+        </div>
+        <el-dropdown-menu slot="dropdown" class="dropdown-header-menu">
+        <el-dropdown-item v-for="item of notifications" :key="item.value" :disabled="size===item.value" :command="item.value">
+            <div class="p-15 notify-item">
+                <div class="flex">
+                    <img :src="item.icon" class="notify-icon"/>
+                    <div>
+                        <p class="m-0">{{ item.description }}</p>
+                        <i class="timestamp">{{ moment(item.timestamp).format('HH:MM DD/MM/YYYY') }}</i>
+                    </div>
                 </div>
             </div>
-        </div>
-      </el-dropdown-item>
-    </el-dropdown-menu>
-  </el-dropdown>
+        </el-dropdown-item>
+        </el-dropdown-menu>
+    </el-dropdown>
 </template>
 
 <script>
